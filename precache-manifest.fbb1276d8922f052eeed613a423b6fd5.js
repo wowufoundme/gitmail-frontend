e101f82ba069dfaef1a9b9e95e0800ad48eb9aa2
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-08-10 13:34:07 +0530

    Updates

---
+++ precache-manifest.fbb1276d8922f052eeed613a423b6fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4541557b759838cddebe0aa29392a59",
+    "url": "/wowufoundme.js.org/gitmail/index.html"
+  },
+  {
+    "revision": "7cdb7162f4c0487d7d27",
+    "url": "/wowufoundme.js.org/gitmail/static/css/main.838cdb01.chunk.css"
+  },
+  {
+    "revision": "f82637c509ee22649ff6",
+    "url": "/wowufoundme.js.org/gitmail/static/js/2.e63d9c94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wowufoundme.js.org/gitmail/static/js/2.e63d9c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cdb7162f4c0487d7d27",
+    "url": "/wowufoundme.js.org/gitmail/static/js/main.b3a2181d.chunk.js"
+  },
+  {
+    "revision": "4c11fe8780043f91c61d",
+    "url": "/wowufoundme.js.org/gitmail/static/js/runtime-main.9cf18bf8.js"
+  }
+]);
