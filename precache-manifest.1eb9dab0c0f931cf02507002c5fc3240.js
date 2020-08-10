5fc9209850e6fe476f03955def4514d9d10e01d9
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-08-10 13:32:01 +0530

    Updates

---
+++ precache-manifest.1eb9dab0c0f931cf02507002c5fc3240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f22ee31d04a3b08d4c726015f40219",
+    "url": "/gitmail/index.html"
+  },
+  {
+    "revision": "7cdb7162f4c0487d7d27",
+    "url": "/gitmail/static/css/main.838cdb01.chunk.css"
+  },
+  {
+    "revision": "f82637c509ee22649ff6",
+    "url": "/gitmail/static/js/2.e63d9c94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gitmail/static/js/2.e63d9c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cdb7162f4c0487d7d27",
+    "url": "/gitmail/static/js/main.b3a2181d.chunk.js"
+  },
+  {
+    "revision": "e43895b7a91c39ae5a3e",
+    "url": "/gitmail/static/js/runtime-main.561a4b71.js"
+  }
+]);
